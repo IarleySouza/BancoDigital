378f32bbf48d2f7193c60c378bd0422a61de4dd3
iarley <iarleysouza1003@gmail.com>
2024-07-02 00:34:17 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+public class Main {
+
+	public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+        cc.depositar(20);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
